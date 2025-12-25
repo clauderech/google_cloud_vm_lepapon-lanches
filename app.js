15c0c099e6ff11175cf263201dbdc715dfbbd101
corech <co.rech@hotmail.com>
2025-12-25 01:33:30 -0300

    atualizacao_20

--- app.js
+++ app.js
@@ -14,19 +14,20 @@ const port = process.env.PORT;
 const verifyToken = process.env.WHATSAPP_VERIFY_TOKEN;
 
 // Route for GET requests
-app.get('/', (req, res) => {
+app.get('/webhook', (req, res) => {
   const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
 
   if (mode === 'subscribe' && token === verifyToken) {
     console.log('WEBHOOK VERIFIED');
     res.status(200).send(challenge);
   } else {
+    console.log('WEBHOOK VERIFICATION FAILED');
     res.status(403).end();
   }
 });
 
 // Route for POST requests
-app.post('/', (req, res) => {
+app.post('/webhook', (req, res) => {
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
   console.log(`\n\nWebhook received ${timestamp}\n`);
   console.log(JSON.stringify(req.body, null, 2));
