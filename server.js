4c6f353f745684990427c955e1cafc76840a098d
corech <co.rech@hotmail.com>
2025-12-25 00:35:49 -0300

    atualizacao_11

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express, { json } from 'express';
-import router from './routes/whatsappRoutes';
+import router from 'routes/whatsappRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
